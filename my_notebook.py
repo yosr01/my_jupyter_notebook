4a378672505532f7c45f91d74c9d80ba52fd5267
yosr01 <62879131+yosr01@users.noreply.github.com>
2023-01-25 12:40:14 +0100

    Add files via upload

---
+++ my_notebook.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Yosr Moalla**
+# I'm a data science enginnering student
+
+# *I am interested in data science because it's an evolving field that is related to many exciting fields*
+
+# ### The following code adds 200$ to a salary
+
+# In[5]:
+
+
+def add_bonus (salary):
+    return salary+200
+add_bonus(4000)
+
+
+# 1. First_list_item
+# 2. Second_list_item
+# 3. Third_list_item
+# 4. Fourth_list_item
+# ---
+# [www.ibm.com](https://www.ibm.com/fr-fr)
+
+# In[ ]:
+
+
+
+
